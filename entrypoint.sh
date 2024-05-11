3be5df27d938348af1cd116e387199a8d41265b7
a.vinogradov <a.vinogradov@bpcbt.com>
2024-05-11 21:19:29 +0300

    upd

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 CONFIG=/etc/shadowsocks-rust/config.json
 
 echo $CONFIG
+echo port $PORT
+echo password $PASSWORD
 
 /bin/sed -i "s|#PORT#|$PORT|g" $CONFIG
 /bin/sed -i "s|#PASSWORD#|$PASSWORD|g" $CONFIG
